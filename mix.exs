f2c2a6d12e34d139ac0c181eb4ac92545db6d9df
Yuri Artemev <i@artemeff.com>
2019-03-20 10:30:44 +0300

    publish to hex

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule MoxEnv.MixProject do
     [
       app: :mox_env,
       version: "0.4.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # package
+      description: description(),
+      package: package()
     ]
   end
 
@@ -20,6 +24,18 @@ defmodule MoxEnv.MixProject do
   defp deps do
     [
       {:mox, "0.4.0"}
+      {:ex_doc, "~> 0.19", only: :dev},
+    ]
+  end
+
+  defp description do
+    "It's your application config but simply mocked with Mox."
+  end
+
+  defp package do
+    [
+      links: %{"GitHub" => "https://github.com/artemeff/mox_env"},
+      licenses: ["MIT"]
     ]
   end
 end
