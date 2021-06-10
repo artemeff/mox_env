0345a41bb5fc377774e3e0b4f43de008e9d52473
Yuri Artemev <i@artemeff.com>
2021-06-10 11:14:57 +0300

    run mix format

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule MoxEnv.MixProject do
       app: :mox_env,
       version: "1.0.0",
       elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
@@ -23,12 +23,12 @@ defmodule MoxEnv.MixProject do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
       {:mox, "~> 1.0.0"},
-      {:ex_doc, "~> 0.24", only: :dev},
+      {:ex_doc, "~> 0.24", only: :dev}
     ]
   end
 
